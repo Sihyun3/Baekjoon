b990e61fbc3bfb0bc01542c85ecc79c64e1c27a7
shlee <yellownyou@gmail.com>
2025-02-26 02:18:25 +0900

    2745 문제 풀이

---
+++ 2745/2745.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int answer = 0;
+        Scanner scanner = new Scanner(System.in);
+        String numberList = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String input = scanner.nextLine();
+
+        String[] split = input.split(" ");
+
+        int numberSystem = Integer.parseInt(split[1]);
+        String number = split[0];
+
+        for (int i = number.length() - 1; i >= 0; i--) {
+            answer += Math.pow(numberSystem, number.length() - 1  - i) * numberList.indexOf(number.charAt(i));
+        }
+
+        System.out.println(answer);
+        scanner.close();
+    }
+}
