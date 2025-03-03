3455ab124c0f0612637e49298a8db72af9140674
shlee <yellownyou@gmail.com>
2025-03-03 21:43:38 +0900

    24723 문제풀이

---
+++ 24723/24723.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int line = Integer.parseInt(br.readLine());
+        double result = Math.pow(2, line);
+
+        System.out.println((int)result);
+
+    }
+}
