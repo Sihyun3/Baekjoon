07bd5f8bcdc273373feb446e428ff3437c477432
shlee <yellownyou@gmail.com>
2025-11-12 14:35:50 +0900

    2231 java 문제 풀이

--- 2231/2231.java
+++ 2231/2231.java
@@ -2,6 +2,26 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String in = scanner.nextLine();
+        int digits = String.valueOf(in).length();
+        int input = Integer.parseInt(in);
+        int result = 0;
+
+        for(int i = (input - (digits * 9)); i < input; i++){
+            int sum = 0;
+            int num = i;
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum + i == input){
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
     }
 }
