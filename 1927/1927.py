780f348bbf9773e49c409bf66c85690a64390657
Sihyun3 <yellownyou@gmail.com>
2025-11-15 14:43:55 +0900

    1927 python 문제풀이

---
+++ 1927/1927.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+heap = []
+for _ in range(0,N):
+    x = int(input())
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else :
+            print(heapq.heappop(heap))
+    else :
+        heapq.heappush(heap,x)
