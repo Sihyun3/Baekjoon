635866571d298117fe5b8260db00867146d3a00e
shlee <yellownyou@gmail.com>
2025-12-05 11:12:11 +0900

    9665 java 문제풀이

---
+++ 9655/9655.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        
+        if (input % 2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+        
+
+        scanner.close();
+    }
+}
